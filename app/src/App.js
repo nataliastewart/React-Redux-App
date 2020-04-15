de5d15a21e8b74474a7f76ef9e601aab8cf76fad
nataliastewart <nataliastewart@users.noreply.github.com>
2020-04-15 15:58:10 -0400

    Starting project -  logo image working

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://cdn11.bigcommerce.com/s-sq9zkarfah/images/stencil/1280x1280/products/116702/204469/Heisenberg-Cool-Face-Breaking-Bad-Vinyl-Decal-Sticker__21529.1511164840.jpg?c=2&imbypass=on"
+          className="App-logo"
+          alt="logo"
+        />
+        <p>Breaking Bad Quotes</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
